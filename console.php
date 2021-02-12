db169e6eb6be45c97e09ccb99477459b02f9b945
ging-dev <gingdz11012001@gmail.com>
2021-02-12 11:13:37 +0700

    Refactoring

--- console.php
+++ console.php
@@ -2,7 +2,9 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use Gingdev\Facebook;
+use Gingdev\Facebook\Facebook;
+use Facebook\FacebookSession;
+use Facebook\FacebookRequestException;
 use Gingdev\Tools\Tanglikefree;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,15 +16,15 @@ use Symfony\Component\Console\Style\SymfonyStyle;
     ->setName('Tanglikefree Tool')
     ->setVersion('1.0.1')
     ->setCode(function (InputInterface $input, OutputInterface $output) {
+        FacebookSession::enableAppSecretProof(false);
         $io = new SymfonyStyle($input, $output->section());
-        $fb = new Facebook();
 
         $section = $output->section();
 
-        $io->title('Gingdev - Tool TangLikeFree v1.0.1');
+        $io->title('Gingdev - Tool TangLikeFree v1.0.2');
 
         try {
-            $fb->setSession('default');
+            $fb = new Facebook('default');
         } catch (\LogicException $e) {
             $io->error('Run `./vendor/bin/console facebook:login default`');
 
@@ -30,8 +32,9 @@ use Symfony\Component\Console\Style\SymfonyStyle;
         }
 
         $task = new Tanglikefree();
-        $response = $fb->get('/me');
-        $user = $response->getGraphNode()
+        $response = $fb->request('GET', '/me')
+            ->execute();
+        $user = $response->getGraphObject()
             ->asArray();
 
         $io->table(
@@ -48,14 +51,13 @@ use Symfony\Component\Console\Style\SymfonyStyle;
             foreach ($data as $post) {
                 $io->text('ID: '.$post->idpost);
                 try {
-                    $fb->post("/{$post->idpost}/likes");
+                    $fb->request('POST', sprintf('/%s/likes', $post->idpost))
+                        ->execute();
 
                     $result = $task->receiveCoins($post->idpost);
                     $io->text('<'.($result->error ? 'comment' : 'info').'>'.$result->messages.'</>');
-
-                    $fb->delete("{$post->idpost}/likes");
-                } catch (\Facebook\Exceptions\FacebookResponseException $e) {
-                    $io->text('<error>Không thể like: '.$post->idpost.'</>');
+                } catch (FacebookRequestException $e) {
+                    $io->text('<error>'.$e->getMessage().'</>');
                 }
                 $progressBar->advance();
             }
